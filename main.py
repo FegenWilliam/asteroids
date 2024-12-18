35ec1032926e68d437d7890377e1b279020050dd
Fegen_William <fegenwilliam@gmail.com>
2024-12-18 14:19:04 +0700

    added player movement and object grouping

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -14,8 +17,10 @@ def main():
                 return
         screen.fill("black")
         dt = clock.tick(60)/1000
-        player.draw(screen)
-        player.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+        for update in updatable:
+            update.update(dt)
         pygame.display.flip()
         
     
